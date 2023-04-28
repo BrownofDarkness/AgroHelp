e3c359bbfb69d965bcd38ed1555fb3973d535df9
Tomdieu <ivantomdio@gmail.com>
2023-04-28 20:46:08 +0100

    updated

--- src/back-end/AgroHelp/core/api_urls.py
+++ src/back-end/AgroHelp/core/api_urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from rest_framework.routers import DefaultRouter
 
 from . import api_views
@@ -7,11 +6,3 @@ router = DefaultRouter()
 router.register('soil_area', api_views.SoilAreaViewSet, basename='soil-area')
 
 urlpatterns = router.urls
-=======
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-# router.register('client', ClientViewSet, basename='client')
-
-urlpatterns = router.urls
->>>>>>> 3880fc5a3bb04beaa287ce7d4a83ca7be2edc793
