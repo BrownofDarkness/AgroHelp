f87f07f0fb7db3efd3ac393bfad2a83495ea5b83
Tomdieu <ivantomdio@gmail.com>
2023-05-17 22:52:17 +0100

    updated

--- src/back-end/AgroHelp/core/api_views.py
+++ src/back-end/AgroHelp/core/api_views.py
@@ -69,6 +69,7 @@ class SoilViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
 
 class SoilAreaViewSet(
     CreateModelMixin,
+    RetrieveModelMixin,
     UpdateModelMixin,
     DestroyModelMixin,
     ListModelMixin,
