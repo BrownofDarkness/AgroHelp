f0610d44ab89b54fc1015386f072e9e2fdbb8ce9
FOMUBAD-BORISTA-FONDI <borista.fomubad@facsciences-uy1.cm>
2023-06-02 22:50:49 +0100

    back

--- src/back-end/microservices/soils_management/core/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import SoilAreaViewSet, SoilViewSet
-
-router = DefaultRouter()
-router.register('soil_area', SoilAreaViewSet, basename='soil-area')
-router.register('soil', SoilViewSet, basename='soil')
-
-urlpatterns = router.urls
